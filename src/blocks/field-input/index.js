3e565e597fabe9f4b1736be30e2f7efe9545e282
tryall1 <genial712@gmail.com>
2023-09-14 10:26:01 +0200

    field masked input added

--- src/blocks/field-input/index.js
+++ src/blocks/field-input/index.js
@@ -1,2 +1,10 @@
 import './field-input.scss';
 
+import IMask from 'imask';
+
+const element = document.getElementById('field__input-masked');
+const maskOptions = {
+  mask: Date,
+  min: new Date(2022, 9, 13),
+};
+const mask = IMask(element, maskOptions);
