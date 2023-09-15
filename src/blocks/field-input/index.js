50da777e5dfd8bf282472eeb12edf71a19a13ce2
tryall1 <genial712@gmail.com>
2023-09-15 16:26:37 +0200

    dropdown and filter date dropdown expander animation

--- src/blocks/field-input/index.js
+++ src/blocks/field-input/index.js
@@ -2,9 +2,41 @@ import './field-input.scss';
 
 import IMask from 'imask';
 
+// Date validation
 const element = document.getElementById('field__input-masked');
 const maskOptions = {
   mask: Date,
   min: new Date(2022, 9, 13),
 };
 const mask = IMask(element, maskOptions);
+
+import AirDatepicker from 'air-datepicker'
+import "air-datepicker/air-datepicker.css"
+
+new AirDatepicker('#date_range', {
+    range: true,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'dd MMM',
+    selectedDates: ['2023-08-19','2023-08-23'],
+    onShow: function(isFinished) {
+      if (isFinished) {
+          document.querySelector('.expander').style.transform = 'rotate(180deg)';
+      }
+    },
+    onHide: function(isFinished) {
+      if (isFinished) {
+          document.querySelector('.expander').style.transform = 'rotate(0deg)';
+      }
+    },
+})
+
+//control arrow direction when calendar is open
+$('#date_range').datepicker({
+}).data('datepicker')
+  .on('show', function() {
+    $('#date_range').addClass('calendar-open');
+    alert('ok');
+  })
+  .on('hide', function() {
+    $('#date_range').removeClass('calendar-open');
+});
--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,3 +1,4 @@
 import './form.scss';
 import '../../blocks/field-heading';
 import '../../blocks/field-input';
+import '../../blocks/dropdown';
