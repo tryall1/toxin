37edbb648bbb60e83924083cb147074dfaa5b37f
tryall1 <genial712@gmail.com>
2023-09-17 17:43:05 +0200

    calendar customization

--- src/blocks/field-input/index.js
+++ src/blocks/field-input/index.js
@@ -1,5 +1,3 @@
-import './field-input.scss';
-
 import IMask from 'imask';
 
 // Date validation
@@ -10,14 +8,23 @@ const maskOptions = {
 };
 const mask = IMask(element, maskOptions);
 
-import AirDatepicker from 'air-datepicker'
-import "air-datepicker/air-datepicker.css"
+let button = {
+  className: 'custom-button-classname',
+  content: 'Применить',
+}
 
+import AirDatepicker from 'air-datepicker'
 new AirDatepicker('#date_range', {
     range: true,
     multipleDatesSeparator: ' - ',
     dateFormat: 'dd MMM',
     selectedDates: ['2023-08-19','2023-08-23'],
+    buttons: ['clear', button],
+    visible: true,
+    prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
+    nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
+
+    
     onShow: function(isFinished) {
       if (isFinished) {
           document.querySelector('.expander').style.transform = 'rotate(180deg)';
@@ -30,13 +37,6 @@ new AirDatepicker('#date_range', {
     },
 })
 
-//control arrow direction when calendar is open
-$('#date_range').datepicker({
-}).data('datepicker')
-  .on('show', function() {
-    $('#date_range').addClass('calendar-open');
-    alert('ok');
-  })
-  .on('hide', function() {
-    $('#date_range').removeClass('calendar-open');
-});
+import "air-datepicker/air-datepicker.css"
+
+import './field-input.scss';
