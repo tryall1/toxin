7edab365c0934489bc5803ece5cb3df5183ccbda
tryall1 <genial712@gmail.com>
2023-09-20 12:36:53 +0200

    calendar code refactoring

--- src/blocks/field-input/index.js
+++ src/blocks/field-input/index.js
@@ -1,4 +1,5 @@
-import IMask from 'imask';
+import IMask from 'imask'
+import AirDatepicker from 'air-datepicker'
 
 // Date validation
 const element = document.getElementById('field__input-masked');
@@ -8,26 +9,31 @@ const maskOptions = {
 };
 const mask = IMask(element, maskOptions);
 
+const first_date = document.getElementById('start_date');
+new IMask(first_date, maskOptions);
+
+const end_date = document.getElementById('end_date');
+new IMask(end_date, maskOptions);
+
+
 let button = {
-  className: 'custom-button-classname',
+  className: 'custom-button-calendar', //no styles in scss
   content: 'Применить',
 }
 
-import AirDatepicker from 'air-datepicker'
 new AirDatepicker('#date_range', {
     range: true,
     multipleDatesSeparator: ' - ',
     dateFormat: 'dd MMM',
+    autoClose: true,
     selectedDates: ['2023-08-19','2023-08-23'],
     buttons: ['clear', button],
-    visible: true,
     prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
     nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
 
-    
     onShow: function(isFinished) {
       if (isFinished) {
-          document.querySelector('.expander').style.transform = 'rotate(180deg)';
+          document.querySelector('.expander').style.transform = 'rotate(180deg)'; //doesn't work correctly
       }
     },
     onHide: function(isFinished) {
@@ -36,6 +42,42 @@ new AirDatepicker('#date_range', {
       }
     },
 })
+
+//Several inputs in date picker
+let startDatepicker = new AirDatepicker('#start_date', {
+  multipleDatesSeparator: ' - ',
+  dateFormat: 'dd.MM.yyyy',
+  buttons: ['clear', button],
+  range: false,
+  prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
+  nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
+
+  //updating minimum date in second calendar input
+  onSelect({date}) {
+    
+    endDatepicker.selectDate(startDatepicker.selectedDates[0]);
+    startDatepicker.hide();
+    endDatepicker.show();
+
+    endDatepicker.update({
+        minDate: date
+    })
+  }
+});
+
+let endDatepicker = new AirDatepicker('#end_date', {
+  dateFormat: 'dd.MM.yyyy',
+  prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
+  nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
+  range: true, 
+  selectedDates: ['2023-08-19'],
+  autoClose: true,
+  onSelect({date}) {
+    if(date.length===2) {
+      endDatepicker.selectDate(endDatepicker.selectedDates[1]); //selecting only one(last) date in input
+    }
+  }
+});
 
 import "air-datepicker/air-datepicker.css"
 
