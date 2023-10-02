e2f67e6f6db177c06db5c7fac2dd029ee8341cc9
tryall1 <genial712@gmail.com>
2023-10-02 17:33:44 +0200

    expandable refactoring

--- src/blocks/checkbox-buttons/index.js
+++ src/blocks/checkbox-buttons/index.js
@@ -1,1 +1,12 @@
 import './checkbox-buttons.scss';
+import '../field-heading/index';
+
+$(document).ready(function() {
+    $('.expandable-buttons').on('click', function() {
+        $(this).toggleClass('expandable-buttons--open');
+    });
+});
+
+$(document).ready(function() {
+    $('.expandable-buttons').eq(1).addClass('expandable-buttons--open');
+});
