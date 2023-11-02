b8609b5e19cd0791a07d65bc70f42ea3709a84c0
tryall1 <genial712@gmail.com>
2023-11-02 14:00:49 +0100

    room-info block

--- src/blocks/room-info/index.js
+++ src/blocks/room-info/index.js
@@ -1,62 +1,28 @@
+import './room-info.scss'
+import '../field-heading/field-heading.scss'
+import '../field-input/index'
+import '../field-input/field-input.scss'
+import '../dropdown/index'
+import '../dropdown/dropdown.scss'
+import '../button/button.scss'
 
-import IMask from 'imask'
-import AirDatepicker from 'air-datepicker'
-import "air-datepicker/air-datepicker.css"
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
 
-//Data mask
-const maskOptions = {
-  mask: Date,
-  min: new Date(2022, 9, 13),
-};
-
-const first_date = document.getElementById('start_date');
-new IMask(first_date, maskOptions);
-
-const end_date = document.getElementById('end_date');
-new IMask(end_date, maskOptions);
-
-const birthday = document.getElementById('birthday');
-
-new IMask(birthday, 
-  {
-    mask: Date,
-  });
 
 //Datepicker for date range and two inputs
 var button = {
-  className: 'custom-button-calendar', 
-  content: 'Применить',
+    className: 'custom-button-calendar', 
+    content: 'Применить',
 }
 
-new AirDatepicker('#date_range', {
-    range: true,
-    multipleDatesSeparator: ' - ',
-    dateFormat: 'dd MMM',
-    autoClose: true,
-    selectedDates: ['2023-08-19','2023-08-23'],
-    buttons: ['clear', button],
-    prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
-    nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
-
-    onShow: function(isFinished) {
-      if (isFinished) {
-          document.querySelector('.expander').style.transform = 'rotate(180deg)'; //doesn't work correctly
-      }
-    },
-    onHide: function(isFinished) {
-      if (isFinished) {
-          document.querySelector('.expander').style.transform = 'rotate(0deg)';
-      }
-    },
-})
-
-var startDate = document.querySelector('#start_date');
+var startDate = document.querySelector('#start_date2');
 
 // Получить значение атрибута placeholder
 var startDateValue = startDate.getAttribute('placeholder');
 
 //Several inputs in date picker
-let startDatepicker = new AirDatepicker('#start_date', {
+let startDatepicker = new AirDatepicker(startDate, {
   multipleDatesSeparator: ' - ',
   dateFormat: 'dd.MM.yyyy',
   selectedDates: startDateValue,
@@ -79,11 +45,12 @@ let startDatepicker = new AirDatepicker('#start_date', {
 });
 
 
-var endDate = document.querySelector('#end_date');
+var endDate = document.querySelector('#end_date2');
+
 // Получить значение атрибута placeholder
 var endDateValue = endDate.getAttribute('placeholder');
 
-let endDatepicker = new AirDatepicker('#end_date', {
+let endDatepicker = new AirDatepicker(endDate, {
   dateFormat: 'dd.MM.yyyy',
   prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
   nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
@@ -98,4 +65,27 @@ let endDatepicker = new AirDatepicker('#end_date', {
   }
 });
 
-import './field-input.scss';
+$(document).ready(function() {
+    // Инициализация iqdropdown
+    var options_guest = {
+        setSelectionText: function setSelectionText(itemCount, totalItems) {
+            if (totalItems === 0) {
+                return 'Сколько гостей';
+            } else {
+                let lastDigit = totalItems % 10;
+                let lastTwoDigits = totalItems % 100;
+                if (lastDigit === 1 && lastTwoDigits !== 11) {
+                    return totalItems + ' гость';
+                } else if ((lastDigit >= 2 && lastDigit <= 4) && !(lastTwoDigits >= 12 && lastTwoDigits <= 14)) {
+                    return totalItems + ' гостя';
+                } else {
+                    return totalItems + ' гостей';
+                }
+            }
+        }
+    }
+
+    $('#dropdown-guest').iqDropdown(options_guest);
+});
+
+
--- src/pages/cards/index.js
+++ src/pages/cards/index.js
@@ -1,4 +1,4 @@
 import './cards.scss'
 import '../../blocks/room-search';
 import '../../blocks/registration';
-
+import '../../blocks/room-info';
