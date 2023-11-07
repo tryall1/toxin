e6cb6ca844290429a47d3f85d20930c733b47825
tryall1 <genial712@gmail.com>
2023-11-07 22:34:13 +0100

    login form added

--- src/blocks/room-info/index.js
+++ src/blocks/room-info/index.js
@@ -9,61 +9,113 @@ import '../button/button.scss'
 import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 
-
-//Datepicker for date range and two inputs
-var button = {
-    className: 'custom-button-calendar', 
-    content: 'Применить',
+function parseDateFromPlaceholder(inputElement) {
+  const dateString = inputElement.getAttribute('placeholder');
+  return dateString ? new Date(dateString.split('.').reverse().join('-')) : null;
 }
 
-var startDate = document.querySelector('#start_date2');
+var startDateInput = document.querySelector('#start_date2');
+var endDateInput = document.querySelector('#end_date2');
 
-// Получить значение атрибута placeholder
-var startDateValue = startDate.getAttribute('placeholder');
+var button = {
+  className: 'custom-button-calendar', 
+  content: 'Применить'
+};
 
-//Several inputs in date picker
-let startDatepicker = new AirDatepicker(startDate, {
-  multipleDatesSeparator: ' - ',
-  dateFormat: 'dd.MM.yyyy',
-  selectedDates: startDateValue,
-  buttons: ['clear', button],
-  range: false,
-  prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
-  nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
+var arrowPrevHtml = '<svg>...</svg>'; // Ваш SVG код для стрелки влево
+var arrowNextHtml = '<svg>...</svg>'; // Ваш SVG код для стрелки вправо
 
-  //updating minimum date in second calendar input
-  onSelect({date}) {
-    
-    endDatepicker.selectDate(startDatepicker.selectedDates[0]);
-    startDatepicker.hide();
-    endDatepicker.show();
+let startDate = parseDateFromPlaceholder(startDateInput);
+let endDate = parseDateFromPlaceholder(endDateInput);
 
-    endDatepicker.update({
-        minDate: date
-    })
-  }
-});
-
-
-var endDate = document.querySelector('#end_date2');
-
-// Получить значение атрибута placeholder
-var endDateValue = endDate.getAttribute('placeholder');
-
-let endDatepicker = new AirDatepicker(endDate, {
-  dateFormat: 'dd.MM.yyyy',
-  prevHtml: '<svg><path d="M16.1755 8.01562V9.98438H3.98801L9.56613 15.6094L8.15988 17.0156L0.144258 9L8.15988 0.984375L9.56613 2.39062L3.98801 8.01562H16.1755Z" fill="#BC9CFF"/></svg>',
-  nextHtml: '<svg><path d="M8.36301 0.984375L16.3786 9L8.36301 17.0156L6.95676 15.6094L12.5349 9.98438H0.347383V8.01562H12.5349L6.95676 2.39062L8.36301 0.984375Z" fill="#BC9CFF"/></svg>',
-  range: true, 
-  selectedDates: endDateValue,
-  autoClose: true,
-  buttons: ['clear', button],
-  onSelect({date}) {
-    if(date.length===2) {
-      endDatepicker.selectDate(endDatepicker.selectedDates[1]); //selecting only one(last) date in input
+let datepicker = new AirDatepicker(startDateInput, {
+    range: true,
+    multipleDatesSeparator: ' - ',
+    dateFormat: 'dd.MM.yyyy',
+    prevHtml: arrowPrevHtml,
+    nextHtml: arrowNextHtml,
+    selectedDates: startDate ? [startDate] : [],
+    autoClose: true,
+    buttons: ['clear', button],
+    onSelect({formattedDate, date}) {
+      if (Array.isArray(formattedDate) && formattedDate.length === 2) {
+        startDateInput.value = formattedDate[0];
+        endDateInput.value = formattedDate[1];
+        console.log(changesum(startDateInput.value, endDateInput.value));
+      } else {
+        console.log(formattedDate)
+        startDateInput.value = formattedDate[0] || '';
+      }
     }
-  }
+
 });
+
+endDateInput.addEventListener('click', function() {
+  let currentStartDate = startDateInput.value;
+
+  //datepicker.clear();
+  if (endDate) {
+    datepicker.selectDate([currentStartDate, endDate].filter(d => d));
+  }
+
+  datepicker.show();
+});
+
+startDateInput.addEventListener('click', function() {
+  let currentStartDate = startDateInput.value ? new Date(startDateInput.value.split('.').reverse().join('-')) : null;
+  let currentEndDate = endDateInput.value ? new Date(endDateInput.value.split('.').reverse().join('-')) : null;
+
+  datepicker.clear();
+  if (currentStartDate) {
+    datepicker.selectDate([currentStartDate, currentEndDate].filter(d => d));
+  }
+
+  datepicker.show();
+});
+
+// Установка начального значения endDateInput, если обе даты предоставлены
+if (startDate && endDate) {
+  endDateInput.value = endDate.toLocaleDateString('ru-RU', { day: '2-digit', month: '2-digit', year: 'numeric' });
+}
+
+function changesum(startDateInput, endDateInput) {
+  const date1 = new Date(startDateInput.split(".").reverse().join("-"));
+  const date2 = new Date(endDateInput.split(".").reverse().join("-"));
+
+  // Разница в миллисекундах
+  const differenceInTime = date2.getTime() - date1.getTime();
+
+  // Преобразование разницы в миллисекундах в дни
+  const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+
+  // Находим необходимые элементы
+  const daysElement = document.querySelector('.room-info__line-left-days');
+  const costElement = document.querySelector('.room-info__cost');
+  const sumElement = document.querySelector('.room-info__line-right');
+  const fullPriceElement = document.querySelector('.room-info__full-price');
+
+  // Проверяем, существуют ли элементы
+  if (daysElement && costElement && sumElement && fullPriceElement) {
+      const days = Math.abs(differenceInDays);
+      daysElement.textContent = days + ' дней';
+
+      // Получаем стоимость за день, очищаем от нечисловых символов и преобразуем в число
+      const costPerDay = parseFloat(costElement.textContent.replace(/[^0-9.]/g, ''));
+
+      // Вычисляем общую сумму
+      const totalSum = costPerDay * days;
+      sumElement.textContent = 'Общая сумма: ' + totalSum + ' ₽';
+
+      // Добавляем 300 рублей и обновляем итоговую цену
+      const fullPrice = totalSum + 300;
+      fullPriceElement.textContent = fullPrice + ' ₽';
+  } else {
+      console.error('Один из элементов не найден');
+  }
+}
+
+
+
 
 $(document).ready(function() {
     // Инициализация iqdropdown
--- src/pages/cards/index.js
+++ src/pages/cards/index.js
@@ -2,3 +2,4 @@ import './cards.scss'
 import '../../blocks/room-search';
 import '../../blocks/registration';
 import '../../blocks/room-info';
+import '../../blocks/login';
